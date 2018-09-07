c5d3ee5998f5b3613a25b248e0e529c0c714bd43
TomoakiTANAKA <tanaka1987@gmail.com>
2018-09-07 18:22:29 +0900

    add-menu

--- custom-index-banner.php
+++ custom-index-banner.php
@@ -9,4 +9,48 @@
   License: GPLv2
  */
 
+add_action('init', 'CustomIndexBanner::init');
+
+class CustomIndexBanner
+{
+    static function init()
+    {
+        return new self();
+    }
+
+    function __construct()
+    {
+        if (is_admin() && is_user_logged_in()) {
+            // メニュー追加
+            add_action('admin_menu', [$this, 'set_plugin_menu']);
+            add_action('admin_sub_menu', [$this, 'set_plugin_sub_menu']);
+
+        }
+    }
+
+    function set_plugin_menu()
+    {
+        add_menu_page(
+            'カスタムバナー',           /* ページタイトル*/
+            'カスタムバナー',           /* メニュータイトル */
+            'manage_options',         /* 権限 */
+            'custom-index-banner',    /* ページを開いたときのURL */
+            [$this, 'show_about_plugin'],       /* メニューに紐づく画面を描画するcallback関数 */
+            'dashicons-format-gallery', /* アイコン see: https://developer.wordpress.org/resource/dashicons/#awards */
+            99                          /* 表示位置のオフセット */
+        );
+    }
+    function set_plugin_sub_menu() {
+
+        add_submenu_page(
+            'es-custom-index',  /* 親メニュー */
+            '設定',
+            '設定',
+            'manage_options',
+            'custom-index-banner-config',
+            [$this, 'show_config_form']);
+    }
+
+} // end of class
 ?>
+
