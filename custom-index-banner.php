31894bd368ca54bd7c9aed086338fb90fa1037f6
TomoakiTANAKA <tanaka1987@gmail.com>
2018-09-07 17:54:50 +0900

    create plugin skelton

---
+++ custom-index-banner.php
@@ -0,0 +1,12 @@
+<?php
+/*
+  Plugin Name: CustomIndexBanner
+  Plugin URI:
+  Description: indexページに表示可能なバナーの設定
+  Version: 1.0.0
+  Author: Tomoaki TANAKA
+  Author URI: https://github.com/TomoakiTANAKA/CustomIndexBanner
+  License: GPLv2
+ */
+
+?>
